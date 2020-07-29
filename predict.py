55e83b42cd29fb6e7066e224db1fe51b133766f4
Rodolphe Vidal <rodolphevdl@gmail.com>
2020-07-29 15:18:41 +0200

    pas fini

--- predict.py
+++ predict.py
@@ -1,6 +1,13 @@
-import pandas as pd 
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sys import path
+%matplotlib inline
 
-train = "upload/train2.csv"
-test = "upload/test2.csv"
+train = pd.read_csv('./upload/train2.csv')
+test = pd.read_csv('./upload/test2.csv')
+df = pd.concat([train, test], ignore_index=1)
+mask = df['SeriousDlqin2yrs'].notnull()
 
-df_train = pd.Datataframe("train2.csv")
+df = df.drop('Unnamed: 0', axis=1)
