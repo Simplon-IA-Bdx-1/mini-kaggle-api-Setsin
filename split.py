18d5d1d8e7e19ddf2f5e268d5aa42f0e0230397d
Rodolphe Vidal <rodolphevdl@gmail.com>
2020-07-29 14:05:34 +0200

    Ajout api

---
+++ split.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+data = pd.read_csv('upload/cs-training.csv', index_col=0)
+
+train2, test2 = train_test_split(data, test_size=0.50, random_state=42)
+
+train2.to_csv('train2.csv')
+test2.to_csv('/test2.csv')
